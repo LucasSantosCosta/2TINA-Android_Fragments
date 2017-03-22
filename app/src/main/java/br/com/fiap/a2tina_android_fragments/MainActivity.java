29d260a90c71d4549a1ab44e67e2b208fc121d11
Lucas Santos <santos.lucas00@hotmail.com>
2017-03-22 11:41:50 -0300

    Criando método para reaproveitar e instanciar os fragments

--- app/src/main/java/br/com/fiap/a2tina_android_fragments/MainActivity.java
+++ app/src/main/java/br/com/fiap/a2tina_android_fragments/MainActivity.java
@@ -1,13 +1,34 @@
 package br.com.fiap.a2tina_android_fragments;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentActivity;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragmentContainer = (FrameLayout) findViewById(R.id.fragment_container);
+    }
+
+    public void alteraFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+    }
+
+    public void exibirCarro1(View view) {
+        alteraFragment(new Carro1Fragment());
+    }
+
+    public void exibirCarro2(View view) {
+        alteraFragment(new Carro2Fragment());
     }
 }
